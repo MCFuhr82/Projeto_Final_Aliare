20ba8075bab271ead25aef22e91900d6c666c659
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2022-05-18 21:58:51 -0300

    created login view

--- ProjetoFinalAliare/Program.cs
+++ ProjetoFinalAliare/Program.cs
@@ -16,7 +16,7 @@ namespace ProjetoFinalAliare
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Frm_Login());
         }
     }
 }
