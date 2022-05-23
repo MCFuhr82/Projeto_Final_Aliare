870882c6a2114d2451895cead144f9a0a5263638
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2022-05-23 11:06:58 -0300

    Select Id clicking on gridview

--- ProjetoFinalAliare/Frm_ConsultarAluno.Designer.cs
+++ ProjetoFinalAliare/Frm_ConsultarAluno.Designer.cs
@@ -71,6 +71,7 @@
             this.dataGridView1.Location = new System.Drawing.Point(12, 123);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(688, 314);
             this.dataGridView1.TabIndex = 6;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
